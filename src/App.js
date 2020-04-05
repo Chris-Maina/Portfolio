3102a59c0e3c52317a0e695e9fcadc5a44251418
Chris Maina <chrismaina@Chris-MacBook.local>
2020-04-06 01:25:10 +0300

    modify App.js and gitignore file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Navbar, Welcome, About, Technologies, Hobbies, Footer } from './components';
+
+import styles from './styles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <Navbar />
+      <Welcome id='welcome' />
+      <About id='about' />
+      <Technologies id='technologies' />
+      <Hobbies id='hobbies' />
+      <Footer id='footer' />
     </div>
   );
 }
